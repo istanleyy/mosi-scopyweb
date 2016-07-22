36535edd20ea4aa35ceceb81b339f6c0b4511633
Stanley Yeh <ihyeh@mosi.com.tw>
2016-07-22 12:30:03 +0800

    Added Django Extensions module

--- scopy/scopy/settings.py
+++ scopy/scopy/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'scope_core.apps.ScopeCoreConfig',
     'rest_framework',
     'djcelery',
