44fd21ab873f76030335e7e8e7973bec8fe69822
Stanley Yeh <ihyeh@mosi.com.tw>
2016-07-22 11:52:57 +0800

    Fixing db model

--- scopy/scope_core/admin.py
+++ scopy/scope_core/admin.py
@@ -4,11 +4,11 @@ from django.contrib import admin
 from .models import Machine
 from .models import Job
 from .models import SessionManagement
-from .models import UserActivity
+#from .models import UserActivity
 from .models import ProductionDataTS
 
 admin.site.register(Machine)
 admin.site.register(Job)
 admin.site.register(SessionManagement)
-admin.site.register(UserActivity)
+#admin.site.register(UserActivity)
 admin.site.register(ProductionDataTS)
