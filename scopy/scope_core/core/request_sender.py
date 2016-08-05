702c6017785ae3b44e9199bf607a905acf8cd4f7
Stanley Yeh <ihyeh@mosi.com.tw>
2016-08-05 21:01:42 +0800

    Bug fix

--- scopy/scope_core/core/request_sender.py
+++ scopy/scope_core/core/request_sender.py
@@ -33,7 +33,7 @@ def sendPostRequest(msg, errHandle=False):
         print '<----- remote response: ' + r.content
         if r.content == 'ServerError:msg sync':
             return None
-        elif r.content == 'ServerMsg:no' or r.content[:11] == 'ServerError':
+        elif r.content == 'ServerMsg:no' or r.content == 'ServerMsg:fail' or r.content[:11] == 'ServerError':
             return False
         else:
             return True
