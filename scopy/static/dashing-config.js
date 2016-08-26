2668439045a16cb0a764480722570b123317325d
Stanley Yeh <ihyeh@mosi.com.tw>
2016-08-26 12:25:34 +0800

    Fix dashing config location

--- scopy/static/dashing-config.js
+++ scopy/static/dashing-config.js
@@ -1,75 +1,51 @@
-/* global $, Dashboard */
+/* Customized dashboard for Scope Device */
 
 var dashboard = new Dashboard();
 
-dashboard.addWidget('clock_widget', 'Clock');
-
-dashboard.addWidget('current_valuation_widget', 'Number', {
+dashboard.addWidget('complete_jobs_widget', 'Number', {
     getData: function () {
         $.extend(this.scope, {
-            title: 'Current Valuation',
-            moreInfo: 'In billions',
+            title: 'Complete Jobs',
+            moreInfo: '',
             updatedAt: 'Last updated at 14:10',
             detail: '64%',
-            value: '$35'
+            value: 35
         });
     }
 });
 
-dashboard.addWidget('buzzwords_widget', 'List', {
+dashboard.addWidget('joblist_widget', 'List', {
     getData: function () {
         $.extend(this.scope, {
-            title: 'Buzzwords',
-            moreInfo: '# of times said around the office',
+            title: 'Job List',
+            moreInfo: 'Pending jobs',
             updatedAt: 'Last updated at 18:58',
-            data: [{label: 'Exit strategy', value: 24},
-                   {label: 'Web 2.0', value: 12},
-                   {label: 'Turn-key', value: 2},
-                   {label: 'Enterprise', value: 12},
-                   {label: 'Pivoting', value: 3},
-                   {label: 'Leverage', value: 10},
-                   {label: 'Streamlininess', value: 4},
-                   {label: 'Paradigm shift', value: 6},
-                   {label: 'Synergy', value: 7}]
+            data: [{label: 'W02347892', value: 600},
+                   {label: 'W12798404', value: 1000},
+                   {label: 'W77200394', value: 800},
+                   {label: 'W34502619', value: 3000},
+                   {label: 'W36650932', value: 1000},
+                   {label: 'W56020872', value: 500},
+                   {label: 'W46552901', value: 1000},
+                   {label: 'W25456410', value: 1000},
+                   {label: 'W55640106', value: 300}]
         });
     }
 });
 
-dashboard.addWidget('convergence_widget', 'Graph', {
+dashboard.addWidget('machine_cycle_widget', 'Graph', {
     getData: function () {
         $.extend(this.scope, {
-            title: 'Convergence',
-            value: '41',
-            moreInfo: '',
-            data: [ 
-                    { x: 0, y: 40 }, 
-                    { x: 1, y: 49 }, 
-                    { x: 2, y: 38 }, 
-                    { x: 3, y: 30 }, 
-                    { x: 4, y: 32 }
+            title: 'Cycle Count',
+            value: '40',
+            moreInfo: '# of machine cycles',
+            data: [
+                    { x: 0, y: 10 },
+                    { x: 1, y: 20 },
+                    { x: 2, y: 30 },
+                    { x: 3, y: 30 },
+                    { x: 4, y: 40 }
                 ]
             });
-    }
-});
-
-dashboard.addWidget('completion_widget', 'Knob', {
-    getData: function () {
-        $.extend(this.scope, {
-            title: 'Completion',
-            updatedAt: 'Last updated at 14:10',
-            detail: 'today 10',
-            value: '35',
-            data: {
-                angleArc: 250,
-                angleOffset: -125,
-                displayInput: true,
-                displayPrevious: true,
-                step: 1,
-                min: 1,
-                max: 99,
-                readOnly: true,
-                format: function(value) { return value + '%'; }
-            }
-        });
     }
 });
