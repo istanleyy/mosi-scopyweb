25ecfdb972020337889bcaf44b61a7879974fa57
Stanley Yeh <ihyeh@mosi.com.tw>
2016-09-13 15:59:41 +0800

    Added log file download support

--- scopy/scope_core/views.py
+++ scopy/scope_core/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import ProductionDataTS
+from .models import ProductionDataTS, Document
 
 def index(request):
     return HttpResponse("Welcome to the Scope Device microserver!")
-    
-# Shows current device information (list of jobs, current running status)
-def info(request):
-    pass
 
 # Shows production history for the job with the given job ID
 def hist(request, job_id):
@@ -16,3 +12,11 @@ def hist(request, job_id):
         return HttpResponse("No record found.")
     else:
         return HttpResponse(job_history)
+
+@login_required
+def getlog(request):
+    response = HttpResponse()
+    url = 'protected/celery_worker.log'
+    response['Content-Type'] = ""
+    response['X-Accel-Redirect'] = url
+    return response
