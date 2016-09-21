3695609f3b6b069ed428cb5a3ec6032114ec866e
Stanley Yeh <ihyeh@mosi.com.tw>
2016-09-21 12:15:16 +0800

    Bug fix

--- scopy/scope_core/views.py
+++ scopy/scope_core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from .models import ProductionDataTS, Job, SessionManagement, Machine
 
 def index(request):
