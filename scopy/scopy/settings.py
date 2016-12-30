687c3de7eec85889c5ccac4bb338d449204d538b
Stanley Yeh <ihyeh@mosi.com.tw>
2016-12-30 18:54:13 +0800

    Correct template path

--- scopy/scopy/settings.py
+++ scopy/scopy/settings.py
@@ -65,7 +65,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates')
+            os.path.join(BASE_DIR, 'templates'),
+            '/home/mosidev/sandbox/scopyweb/scopy/static/templates/dashing'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
