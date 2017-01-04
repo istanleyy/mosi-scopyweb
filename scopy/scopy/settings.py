2e4518638afc6a7e23fefb2cf62e6879c2d3bae2
Stanley Yeh <ihyeh@mosi.com.tw>
2017-01-04 12:01:32 +0800

    Fix dashing settings

--- scopy/scopy/settings.py
+++ scopy/scopy/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print BASE_DIR
 
 
 # Quick-start development settings - unsuitable for production
@@ -65,8 +66,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
-            '/home/mosidev/sandbox/scopyweb/scopy/static/templates/dashing'
+            os.path.join(BASE_DIR, 'templates')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -219,7 +219,7 @@ DASHING = {
     'INSTALLED_WIDGETS': (
         'number', 
         'list', 
-        'graph',
+        'scopegraph',
         'scopedevice',
     ),
     'PERMISSION_CLASSES': (
