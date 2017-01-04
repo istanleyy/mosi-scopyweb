c460b07bc71308036b63e4be3bc4e3da22d3a18d
Stanley Yeh <ihyeh@mosi.com.tw>
2017-01-04 16:41:45 +0800

    Fix settings

--- scopy/scopy/settings.py
+++ scopy/scopy/settings.py
@@ -148,10 +148,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
 # Additional djcelery settings
 
 import djcelery
