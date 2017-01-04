b77c9eeb5b732793d0f0e6b862d174149c34130c
Stanley Yeh <ihyeh@mosi.com.tw>
2017-01-04 12:07:32 +0800

    Fix template path

--- scopy/scopy/settings.py
+++ scopy/scopy/settings.py
@@ -14,8 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print BASE_DIR
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -66,7 +64,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates')
+            os.path.join(BASE_DIR, '/static/templates')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
