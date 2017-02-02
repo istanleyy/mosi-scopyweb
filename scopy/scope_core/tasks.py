a21a346aa2748c06a73c2a2f36cda35cc805db38
Stanley Yeh <ihyeh@mosi.com.tw>
2017-02-02 17:42:15 +0800

    Debug

--- scopy/scope_core/tasks.py
+++ scopy/scope_core/tasks.py
@@ -1,10 +1,10 @@
 """Celery task definition for scope_core app"""
 from __future__ import absolute_import, unicode_literals
 
-from celery import Celery
 from celery.utils.log import get_task_logger
 from django_celery_beat.models import PeriodicTask, IntervalSchedule
 from django.core.cache import cache
+#from scopy.celery import app
 from scope_core.core import job_control
 
 LOGGER = get_task_logger(__name__)
@@ -13,8 +13,6 @@ P_PRIOR_MID = 90
 P_PRIOR_LOW = 120
 LOCK_EXPIRE = 60
 LOCK_ID = "shared-lock"
-
-app = Celery()
 
 def init_tasks():
     """Initialize schedules and tasks
@@ -34,7 +32,7 @@ def init_tasks():
         every=P_PRIOR_MID,
         period=IntervalSchedule.SECONDS,
     )
-    firstTask = PeriodicTask.objects.get_or_create(
+    PeriodicTask.objects.get_or_create(
         interval=schedule_high,
         name='Polling device status',
         task='scope_core.tasks.poll_status_task',
@@ -49,23 +47,7 @@ def init_tasks():
         name='Polling production metrics',
         task='scope_core.tasks.poll_metrics_task',
     )
-    firstTask.delay()
-"""
-@app.on_after_configure.connect
-def setup_periodic_tasks(sender, **kwargs):
-    # Calls test('hello') every 10 seconds.
-    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
 
-    # Calls test('world') every 30 seconds
-    sender.add_periodic_task(30.0, test.s('world'), expires=10)
-
-    # Executes every Monday morning at 7:30 a.m.
-    sender.add_periodic_task(
-        crontab(hour=7, minute=30, day_of_week=1),
-        test.s('Happy Mondays!'),
-    )
-"""
-@app.task
 def poll_status_task():
     """Task to poll device status"""
     global LOCK_ID
@@ -83,7 +65,6 @@ def poll_status_task():
     else:
         LOGGER.info("Blocked: previous task not finished yet! ({})".format(LOCK_ID))
 
-@app.task
 def poll_alarm_task():
     """Task to poll alarm status"""
     global LOCK_ID
@@ -101,7 +82,6 @@ def poll_alarm_task():
     else:
         LOGGER.info("Blocked: previous task not finished yet! ({})".format(LOCK_ID))
 
-@app.task
 def poll_metrics_task():
     """Task to poll production metrics"""
     global LOCK_ID
